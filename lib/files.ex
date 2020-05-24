fcb19f4622481e4e8a6dbff25979d04856e4631d
Timoty Granziero <timoty.granziero@gmail.com>
2020-05-24 22:36:21 +0200

    Add files practicing (page 99)

---
+++ lib/files.ex
@@ -0,0 +1,38 @@
+defmodule Files do
+  def lines_lengths!(path) do
+    path
+    |> file_stream!
+    |> Enum.map(&String.length/1)
+  end
+
+  def longest_line_length!(path) do
+    path
+    |> file_stream!
+    |> Stream.map(&String.length/1)
+    |> Enum.max
+  end
+
+  def longest_line!(path) do
+    path
+    |> file_stream!
+    |> Enum.sort(&(byte_size(&1) > byte_size(&2)))
+    |> List.first
+  end
+
+  def words_per_line!(path) do
+    path
+    |> file_stream!
+    |> Enum.map(&length(String.split(&1)))
+  end
+
+  defp without_newline_character(str) do
+    String.replace(str, "\n", "")
+  end
+
+  defp file_stream!(path) do
+    path
+    |> File.stream!
+    |> Stream.map(&without_newline_character/1)
+  end
+
+end
---
+++ test/files_test.exs
@@ -0,0 +1,21 @@
+defmodule FilesTest do
+  use ExUnit.Case
+  doctest Files
+
+  test "longest line length" do
+    assert Files.longest_line_length!("test/resources/some_file.txt") == 16
+  end
+
+  test "lines lenghts" do
+    assert Files.lines_lengths!("test/resources/some_file.txt") == [4, 10, 16, 0, 5]
+  end
+
+  test "longest line content" do
+    assert Files.longest_line!("test/resources/some_file.txt") == "some other words"
+  end
+
+  test "words per line" do
+    assert Files.words_per_line!("test/resources/some_file.txt") == [1, 2, 3, 0, 1]
+  end
+
+end
