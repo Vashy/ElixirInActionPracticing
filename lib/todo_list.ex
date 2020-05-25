562196cb857938d5a4331fcbef614af23e2db73b
Timoty Granziero <timoty.granziero@gmail.com>
2020-05-25 17:46:38 +0200

    Adding Fraction module

--- lib/todo_list.ex
+++ lib/todo_list.ex
@@ -14,10 +14,7 @@ defmodule MultiMap do
   def new, do: %{}
 
   def put(map, key, value) do
-    Map.update(map, key,
-      [value],
-      &[value | &1]
-    )
+    Map.update(map, key, [value], &[value | &1])
   end
 
   def get(map, key), do: Map.get(map, key)
---
+++ test/fraction_test.exs
@@ -0,0 +1,19 @@
+defmodule FractionTest do
+  use ExUnit.Case
+  doctest Fraction
+
+  test "new" do
+    assert Fraction.new(1, 2) === %Fraction{a: 1, b: 2}
+  end
+
+  test "value" do
+    assert Fraction.new(2, 1) |> Fraction.value === 2.0
+    assert Fraction.new(3, 2) |> Fraction.value === 1.5
+  end
+
+  test "add" do
+    import Fraction
+    assert add(new(2, 1), new(3, 1)) === %Fraction{a: 5, b: 1}
+    assert add(new(3, 2), new(3, 1)) === %Fraction{a: 9, b: 2}
+  end
+end
